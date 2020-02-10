928c3460b0bd6ff411404d17b9003863c284f462
jerem <jeremie.cyrille@icloud.com>
2020-02-10 11:56:55 -0500

    dev2

---
+++ src/dev2.java
@@ -0,0 +1,23 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class house {
+	String adress;
+	int Nb_piece;
+	double Price;
+	Date myDate =new Date(2014);
+	enum Types {
+		jumelée,
+		duplex, 
+		triplex,
+		individuelle
+		}
+    public static void main (String[] args) {
+System.out.println();
+}
+    
+}
